569f9afbf4d7978b71c0f325189e5c2c55222a95
Greg Schechter <diabolomaster@gmail.com>
2016-01-08 15:37:29 -0800

    Add the rest of the apis

--- src/index.js
+++ src/index.js
@@ -29,17 +29,76 @@ var ebird = {
             },
 
             geo_spp: {
-
+                recent: standardGet('/data/obs/geo_spp/recent'),
             },
+
+            hotspot: {
+                recent: standardGet('/data/obs/hotspot/recent'),
+            },
+
+            hotspot_spp: {
+                recent: standardGet('/data/obs/hotspot_spp/recent'),
+            },
+
+            loc: {
+                recent: standardGet('/data/obs/loc/recent'),
+            },
+
+            loc_spp: {
+                recent: standardGet('/data/obs/loc_spp/recent'),
+            },
+
+            region: {
+                recent: standardGet('/data/obs/region/recent'),
+            },
+
+            region_spp: {
+                recent: standardGet('/data/obs/region_spp/recent'),
+            },
+        },
+
+        notable: {
+            geo: {
+                recent: standardGet('/data/notable/geo/recent'),
+            },
+
+            hotspot: {
+                recent: standardGet('/data/notable/hotspot/recent'),
+            },
+
+            loc: {
+                recent: standardGet('/data/notable/loc/recent'),
+            },
+
+            region: {
+                recent: standardGet('/data/notable/region/recent'),
+            },
+        },
+
+        nearest: {
+            geo_spp: {
+                recent: standardGet('/data/nearest/geo_spp/recent'),
+            }
         },
     },
 
     product: {
-
+        obs: {
+            hotspot: {
+                recent: standardGet('/product/obs/hotspot/recent'),
+            },
+        },
     },
 
     ref: {
         taxa: standardGet('/ref/taxa/ebird'),
+
+        hotspot: {
+            region: standardGet('/ref/hotspot/region'),
+            geo: standardGet('/ref/hotspot/geo'),
+        },
+
+        location: standardGet('/ref/location'),
     },
 };
 
