f6727ede5682ecd22d148d5434cfb363422a3c02
Greg Schechter <gregthebusker@gmail.com>
2018-11-24 11:35:26 -0800

    Fix tests and upgrade taxonomy to 2.0

--- src/index.js
+++ src/index.js
@@ -1,109 +1,132 @@
-var axios = require('axios');
+var axios = require("axios");
 
-var protocol = 'http:';
-var baseUrl = `${protocol}//ebird.org/ws1.1`
+var protocol = "http:";
+var baseUrl = `${protocol}//ebird.org/ws1.1`;
 var defaultParams = {
-    fmt: 'json',
+  fmt: "json"
 };
 
-var standardGet = (path) => {
-    return (params) => {
-        return axios.get(`${baseUrl}${path}`, {
-            params: {
-                ...defaultParams,
-                ...params,
-            },
-        }).then((response) => {
-            return response.data;
-        }).catch((response) => {
-            throw response.data;
-        });
-    };
+var standardGet = path => {
+  return params => {
+    return axios
+      .get(`${baseUrl}${path}`, {
+        params: {
+          ...defaultParams,
+          ...params
+        }
+      })
+      .then(response => {
+        return response.data;
+      })
+      .catch(response => {
+        throw response.data;
+      });
+  };
+};
+
+var standardGet2 = path => {
+  return params => {
+    return axios
+      .get(`https://ebird.org/ws2.0${path}`, {
+        params: {
+          ...defaultParams,
+          ...params
+        }
+      })
+      .then(response => {
+        return response.data;
+      })
+      .catch(response => {
+        throw response.data;
+      });
+  };
 };
 
 var ebird = {
-    data: {
-        obs: {
-            geo: {
-                recent: standardGet('/data/obs/geo/recent'),
-            },
+  data: {
+    obs: {
+      geo: {
+        recent: standardGet("/data/obs/geo/recent")
+      },
 
-            geo_spp: {
-                recent: standardGet('/data/obs/geo_spp/recent'),
-            },
+      geo_spp: {
+        recent: standardGet("/data/obs/geo_spp/recent")
+      },
 
-            hotspot: {
-                recent: standardGet('/data/obs/hotspot/recent'),
-            },
+      hotspot: {
+        recent: standardGet("/data/obs/hotspot/recent")
+      },
 
-            hotspot_spp: {
-                recent: standardGet('/data/obs/hotspot_spp/recent'),
-            },
+      hotspot_spp: {
+        recent: standardGet("/data/obs/hotspot_spp/recent")
+      },
 
-            loc: {
-                recent: standardGet('/data/obs/loc/recent'),
-            },
+      loc: {
+        recent: standardGet("/data/obs/loc/recent")
+      },
 
-            loc_spp: {
-                recent: standardGet('/data/obs/loc_spp/recent'),
-            },
+      loc_spp: {
+        recent: standardGet("/data/obs/loc_spp/recent")
+      },
 
-            region: {
-                recent: standardGet('/data/obs/region/recent'),
-            },
+      region: {
+        recent: standardGet("/data/obs/region/recent")
+      },
 
-            region_spp: {
-                recent: standardGet('/data/obs/region_spp/recent'),
-            },
-        },
-
-        notable: {
-            geo: {
-                recent: standardGet('/data/notable/geo/recent'),
-            },
-
-            hotspot: {
-                recent: standardGet('/data/notable/hotspot/recent'),
-            },
-
-            loc: {
-                recent: standardGet('/data/notable/loc/recent'),
-            },
-
-            region: {
-                recent: standardGet('/data/notable/region/recent'),
-            },
-        },
-
-        nearest: {
-            geo_spp: {
-                recent: standardGet('/data/nearest/geo_spp/recent'),
-            }
-        },
+      region_spp: {
+        recent: standardGet("/data/obs/region_spp/recent")
+      }
     },
 
-    product: {
-        obs: {
-            hotspot: {
-                recent: standardGet('/product/obs/hotspot/recent'),
-            },
-        },
+    notable: {
+      geo: {
+        recent: standardGet("/data/notable/geo/recent")
+      },
+
+      hotspot: {
+        recent: standardGet("/data/notable/hotspot/recent")
+      },
+
+      loc: {
+        recent: standardGet("/data/notable/loc/recent")
+      },
+
+      region: {
+        recent: standardGet("/data/notable/region/recent")
+      }
     },
 
-    ref: {
-        taxa: standardGet('/ref/taxa/ebird'),
+    nearest: {
+      geo_spp: {
+        recent: standardGet("/data/nearest/geo_spp/recent")
+      }
+    }
+  },
 
-        hotspot: {
-            region: standardGet('/ref/hotspot/region'),
-            geo: standardGet('/ref/hotspot/geo'),
-        },
+  product: {
+    obs: {
+      hotspot: {
+        recent: standardGet("/product/obs/hotspot/recent")
+      }
+    }
+  },
 
-        location: (params) => {
-            var func = params.match ? standardGet('/ref/location/find') : standardGet('/ref/location/list');
-            return func(params);
-        },
-        region: standardGet('/ref/region/find'),
+  ref: {
+    taxa: standardGet2("/ref/taxonomy/ebird"),
+
+    hotspot: {
+      region: standardGet("/ref/hotspot/region"),
+      geo: standardGet("/ref/hotspot/geo")
     },
+
+    location: params => {
+      var func = params.match
+        ? standardGet("/ref/location/find")
+        : standardGet("/ref/location/list");
+      return func(params);
+    },
+    region: standardGet("/ref/region/find")
+  }
 };
 
 module.exports = ebird;
--- test/test.js
+++ test/test.js
@@ -1,44 +1,50 @@
-var jsdom = require('mocha-jsdom')
-var assert = require('assert');
+var assert = require("assert");
 
-describe('ebird tests', () => {
-    jsdom()
+describe("ebird tests", () => {
+  it("refs/taxa should return results with a standardGet call", done => {
+    var ebird = require("../lib/index.js");
+    ebird.ref
+      .taxa()
+      .then(data => {
+        assert.equal(true, data.length > 0);
+        done();
+      })
+      .catch(error => {
+        console.log(error);
+        throw error;
+      });
+  });
 
-    it('refs/taxa should return results with a standardGet call', (done) => {
-        var ebird = require('../lib/index.js');
-        ebird.ref.taxa().then((data) => {
-            assert.equal(true, data.length > 0);
-            done();
-        }).catch((error) => {
-            console.log(error);
-            throw error;
-        });
-    });
+  it("refs/location should return results with list", done => {
+    var ebird = require("../lib/index.js");
+    ebird.ref
+      .location({
+        rtype: "country"
+      })
+      .then(data => {
+        assert.equal(253, data.length);
+        done();
+      })
+      .catch(error => {
+        console.log(error);
+        throw error;
+      });
+  });
 
-    it('refs/location should return results with list', (done) => {
-        var ebird = require('../lib/index.js');
-        ebird.ref.location({
-            rtype: 'country',
-        }).then((data) => {
-            assert.equal(253, data.length);
-            done();
-        }).catch((error) => {
-            console.log(error);
-            throw error;
-        });
-    });
-
-    it('refs/location should return results with find', (done) => {
-        var ebird = require('../lib/index.js');
-        ebird.ref.location({
-            rtype: 'country',
-            match: 'stan',
-        }).then((data) => {
-            assert.equal(8, data.length);
-            done();
-        }).catch((error) => {
-            console.log(error);
-            throw error;
-        });
-    });
+  it("refs/location should return results with find", done => {
+    var ebird = require("../lib/index.js");
+    ebird.ref
+      .location({
+        rtype: "country",
+        match: "stan"
+      })
+      .then(data => {
+        assert.equal(8, data.length);
+        done();
+      })
+      .catch(error => {
+        console.log(error);
+        throw error;
+      });
+  });
 });
